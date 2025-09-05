417ea1022506a422f7b42eac65807cd6ce8e78bf
Harshitha Annam <harshithaannam149@gmail.com>
2025-09-05 12:43:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Head from "./components/Header";
+import Header from "./components/Header";
 import Body from "./components/Body";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import RestoMenu from "./components/RestoMenu";
-import { createHashRouter, Outlet, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router";
 
 const App = () => {
   return (
     <>
-      <Head />
+      <Header />
       <Outlet />
     </>
   );
 };
 
-const appRouter = createHashRouter([
+const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <App />,
